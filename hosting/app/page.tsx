14e03f815b521a123b3e7e8793f344e5b8bafca2
Bailey Mendel <bailey.mendel98@gmail.com>
2024-04-01 17:39:58 -0400

    fix to random order of colums on frontend

--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
 
@@ -10,37 +8,30 @@ import { db } from "./firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 export default function Page() {
-
   const [rowData, setRowData] = useState([]);
   const [columnDefs, setColumnDefs] = useState([]);
 
   const getData = async () => {
-    // Get all docs from "liquidbiospy1" then...
-    await getDocs(collection(db, "liquidbiopsy2")).then((querySnapshot) => {
-      // map the docs into a list of objects
-      const newData = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      // Save the docs into the react state
-      setRowData(newData);
+    const querySnapshot = await getDocs(collection(db, "liquidbiopsy2"));
+    const orderedQuerySnapshot = querySnapshot.docs.sort((a, b) => a.data().timestamp - b.data().timestamp);
+  
+    const newData = orderedQuerySnapshot.map((doc) => ({
+      id: doc.id,
+      ...(doc.data() || {}),
+    }));
 
-      // To dynamically determine columns, we'll iterate through each row...
-      const newColumnDefs = newData.reduce((accumulator, row) => {
-        // Then iterate through each key
-        Object.keys(row).forEach((key) => {
-          // Check if we already have a column for this key
-          if (!accumulator.includes(key)) {
-            // if not, then add the key to our list of colums
-            accumulator.push(key);
-          }
-        });
-        // return the flat list of columns. i.e. [id, company, target]
-        return accumulator;
-      }, []);
-      // then convert the flat list to the nested struct ag-grid expects
-      setColumnDefs(newColumnDefs.map(key => ({ 'field': key })));
-    });
+    setRowData(newData);
+
+    const newColumnDefs = newData.reduce((accumulator, row) => {
+      Object.keys(row).forEach((key) => {
+        if (!accumulator.includes(key)) {
+          accumulator.push(key);
+        }
+      });
+      return accumulator;
+    }, []);
+
+    setColumnDefs(newColumnDefs.map((key) => ({ field: key })));
   };
 
   useEffect(() => {
@@ -48,27 +39,21 @@ export default function Page() {
   }, []);
 
   useEffect(() => {
-    // Define your desired column order here
-    const orderedColumns = ['TEST NAME', 'COMPANY', 'LINK', 'Action (Coded)', 'Cancer Type (Coded)', 'FLUID TYPE', 'FDA Appr?', 'CLIA/CAP cert?', 'Target (Coded)']; // Example order
-    console.log("Ordered Columns:", orderedColumns);
-    console.log("Column Definitions:", columnDefs);
-    // Modify columnDefs to set the order that is needed
+    const orderedColumns = ['TEST NAME', 'COMPANY', 'LINK', 'Action (Coded)', 'Cancer Type (Coded)', 'FLUID TYPE', 'FDA Appr?', 'CLIA/CAP cert?', 'Target (Coded)'];
     setColumnDefs(columnDefs => {
-      const orderedDefs = columnDefs.slice(); // Create a copy
+      const orderedDefs = columnDefs.slice();
       orderedDefs.sort((a, b) => {
         return orderedColumns.indexOf(a.field) - orderedColumns.indexOf(b.field);
       });
       return orderedDefs;
-    } );
-  }, [columnDefs]); // Trigger when columnDefs change
-
+    });
+  }, [columnDefs]);
 
   return (
-    <div
-      className={"ag-theme-quartz-dark"}
-      style={{ width: "100vw", height: "100vh" }}
-    >
+    <div className={"ag-theme-quartz-dark"} style={{ width: "100vw", height: "100vh" }}>
       <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
     </div>
   );
 }
+
+
