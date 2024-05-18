82339be8e1417fef5fe7bc2be195d96f6dfbc322
Bailey Mendel <bailey.mendel98@gmail.com>
2024-05-18 19:02:53 -0400

    wrapper fix

--- hosting/app/pages/wrapper.tsx
+++ hosting/app/pages/wrapper.tsx
@@ -1,9 +1,9 @@
-// src/pages/index.tsx
+// src/pages/wrapper.tsx
 import React from 'react';
-import Page from "/Users/baileymendel/Desktop/OncAssistdesktop/OncAssist/hosting/app/page.tsx"; // Adjust the path to where your page.tsx is located
+import Page from '../page'; // Adjust the path to where your page.tsx is located
 
 const HomePage = () => {
   return <Page />;
-};
+}
 
 export default HomePage;
